ad5c32ac06ebda4866bab1006ad02cfd60f214df
TANABE Takashi <narinari.t@gmail.com>
2016-04-08 00:49:39 +0900

    Add concurrent calc field with bug.

--- src/Main.purs
+++ src/Main.purs
@@ -2,14 +2,17 @@ module Main where
 
 import Prelude
 import Math (max)
-import Data.Array ((..), zip, head, length, concat)
+import Data.Array ((..), (:), zipWith, head, length, concat, take, drop)
 import Data.Int (toNumber)
 import Data.Maybe (Maybe(..), fromMaybe)
-import Data.Tuple (Tuple(..))
-import Data.Foldable (sequence_)
+import Data.Traversable (for)
+import Data.Foldable (sequence_, fold)
 import Data.Nullable (toMaybe)
 import Control.Bind (join)
 import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Console (CONSOLE)
+import Control.Monad.Cont.Trans (runContT)
+import Control.Parallel (inParallel, runParallel)
 import DOM (DOM)
 import DOM.Event.EventTarget (eventListener, addEventListener)
 import DOM.Event.EventTypes (click)
@@ -22,11 +25,11 @@ import DOM.Node.Types (ElementId(..), elementToEventTarget)
 import DOM.Timer (Timer, delay)
 import Graphics.Canvas
 import Color (toHexString, rgb)
-import LifeGame (Game, Field, cross, next)
+import LifeGame (Game, Field, next)
 
 gliderGun :: Field
 gliderGun = [
-   [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+   [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0]
   ,[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
   ,[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
   ,[0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
@@ -58,40 +61,49 @@ gliderGun = [
   ,[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
   ,[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]
 
-main :: forall e. Eff (canvas :: Canvas, timer :: Timer, dom :: DOM | e) Unit
+main :: forall e. Eff (console :: CONSOLE, canvas :: Canvas, timer :: Timer, dom :: DOM | e) Unit
 main = do
   document <- window >>= document
   Just startButton <- toMaybe <$> getElementById (ElementId "start") (htmlDocumentToNonElementParentNode document)
   addEventListener click (eventListener startLoop) true (elementToEventTarget startButton)
 
-startLoop :: forall e. Event -> Eff (canvas :: Canvas, timer :: Timer | e) Unit
+startLoop :: forall e. Event -> Eff (console :: CONSOLE, canvas :: Canvas, timer :: Timer | e) Unit
 startLoop _ = do
   Just canvas <- getCanvasElementById "canvas"
   ctx <- getContext2D canvas
   canvasHeight <- getCanvasHeight canvas
   canvasWidth <- getCanvasWidth canvas
-  let initialField = createField $ concat [gliderGun, gliderGun]
-      cellSize = max (toNumber $ initialField.h) (toNumber initialField.w)
+  let initialGame = createGame $ concat [gliderGun, gliderGun]
+      cellSize = max (toNumber $ initialGame.h) (toNumber initialGame.w)
       cellRect = {x: 0.0, y: 0.0, w: canvasWidth / cellSize, h: canvasHeight / cellSize}
-      loop age field
+      ranges = chunks 2 $ 0..(initialGame.h - 1)
+      loop age game
         | age == 0 = pure unit
         | otherwise = do
             clearRect ctx {x: 0.0, y: 0.0, w: canvasWidth, h: canvasHeight}
-            drawCells ctx cellRect field
-            void $ delay 25 (loop (age - 1)) $ next field
-  loop 500 initialField
+            drawCells ctx cellRect game
+            flip runContT (\f -> void $ delay 20 (loop (age - 1)) $ game {field = f}) $
+              runParallel $ fold <$> (for ranges (inParallel <<< pure <<< next game))
+  loop 500 initialGame
   where
-    createField base = { h: length base, w: fromMaybe 0 $ length <$> head base, field: base}
+    createGame base = { h: length base,
+                        w: fromMaybe 0 $ length <$> head base,
+                        field: base}
 
 drawCells :: forall e. Context2D -> Rectangle -> Game -> Eff (canvas :: Canvas | e) Unit
-drawCells ctx cellRect field =
-  sequence_ $ join $ draw <$> cross (Tuple id (zip (0..999))) <$> zip (0..999) field.field
+drawCells ctx cellRect game =
+sequence_ $ join $ zipWith (\row cols -> zipWith (draw row) (0..game.w) cols) (0..game.h) game.field
   where
-    draw (Tuple row cols) =  draw' <$> cols
-      where
-        draw' (Tuple col target)
-          | target == 0 = pure unit
-          | otherwise = void $ do
-            newCtx <- color ctx target
-            fillPath newCtx $ rect newCtx $ cellRect {x = (toNumber col) * cellRect.w, y = toNumber row * cellRect.h}
-          where color ctx age = (rgb (255 - age) age (255 - age) # toHexString) `setFillStyle` ctx
+    draw row col target
+      | target == 0 = pure unit
+      | otherwise = void $ do
+        newCtx <- color ctx target
+        fillPath newCtx $ rect newCtx $ cellRect {x = (toNumber col) * cellRect.w, y = toNumber row * cellRect.h}
+    color ctx age = (rgb (255 - age) age 140 # toHexString) `setFillStyle` ctx
+
+chunks :: forall a. Int -> Array a -> Array (Array a)
+chunks 0 arr = []
+chunks 1 arr = [arr]
+chunks i arr = chunks' (length arr / i) arr where
+  chunks' i [] = []
+  chunks' i l  = take i l : chunks' i (drop i l)
